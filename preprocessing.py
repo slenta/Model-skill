54d72cd4dbff98c8dc74da1a56a8ee992640b2b2
slenta <simon.lentz@yahoo.de>
2022-05-17 15:54:11 +0200

    plot

--- preprocessing.py
+++ preprocessing.py
@@ -123,7 +123,7 @@ class ensemble_means(object):
             time = ds.time
             ds['time'] = nc.num2date(time[:],time.units)
             print(ds.time)
-            ds = ds.sel(time=slice(str(self.start_year + 1) + '-01', str(self.end_year) + '-12'))
+            ds = ds.sel(time=slice(str(self.start_year + 1) + '-01-16', str(self.end_year) + '-12-16'))
             print(ds.time)
             var = ds[self.variable]
 
