f6f42b78e5df4946e1845b13a59f56e02bef29f7
slenta <simon.lentz@yahoo.de>
2022-05-17 20:21:28 +0200

    plot

--- leadyear.py
+++ leadyear.py
@@ -192,7 +192,7 @@ class ly_series(object):
         ax.plot(x, hist_ly_ts, 'x')
         ax.set_xlabel('Leadyears')
         ax.set_ylabel('Anomal Correlation')
-        ax.set_xticklabels(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '2-5', '2-9'])
+        ax.set_xticklabels(['1', '2', '3', '4', '5', '6', '7', '8', '9', '2-5', '2-9'])
         ax.grid()
         fig.suptitle('Hindcast Correlation by Lead Year: 1960-2013')
         plt.savefig('Leadyear_timeseries_1960_2013.pdf')
