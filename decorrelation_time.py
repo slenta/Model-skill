79260fa8e5640d20b0ee1ef58626cb271a46d73a
slenta <simon.lentz@yahoo.de>
2022-05-23 12:36:15 +0200

    new plots, cleanup

--- decorrelation_time.py
+++ decorrelation_time.py
@@ -41,6 +41,7 @@ class decorrelation_time(object):
                     var_mean = var[:, i, j]
                 #calculate autocorrelation: autocorrelation[k] is correlation at lag k, throw out lag 0
                 autocor = sm.tsa.acf(var_mean)[1:]
+                print(ac[:, i, j].shape)
                 ac[:, i, j] = autocor
 
                 #calculate decorrelation time for each 
