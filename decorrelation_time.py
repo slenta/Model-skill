763d5ac2deb1ee3b28d55bb740516f7ef6d4787a
slenta <simon.lentz@yahoo.de>
2022-05-29 18:19:51 +0200

    new plots, cleanup

--- decorrelation_time.py
+++ decorrelation_time.py
@@ -61,6 +61,7 @@ class decorrelation_time(object):
 
         f = h5.File(cfg.tmp_path + 'decorrelation_time_' + self.name + '.hdf5', 'w')
         decor = f.get('decorrelation_time')
+        decor = np.array(decor)
         
         plt.figure(figsize=(8, 5))
         plt.imshow(decor)
