ed4732bf74b3a3b26935dad162c498b36cbdcd22
slenta <simon.lentz@yahoo.de>
2022-05-29 18:17:12 +0200

    new plots, cleanup

--- create_ly_series.py
+++ create_ly_series.py
@@ -21,12 +21,12 @@ ly.ly_series()
 
 #plot decorrelation time for HadISSTs
 #define threshold for decorrelation mask
-threshold = 1
+#threshold = 1
 HadIsst = get_variable(path = cfg.observation_path, start_year = 1960, end_year = 2021)
 HadIsst = HadIsst.__getitem__()
 decor = decorrelation_time(HadIsst, del_t=8, threshold=threshold, name='HadIsst')
-dc, mask = decor.__getitem__()
-decorrelation_time.plot()
+#dc, mask = decor.__getitem__()
+decor.plot()
 
 #plot correlation between ocean heat content and ssts
 IAP_Ohc = get_variable(path = cfg.ohc_path, start_year=1960, end_year=2021)
