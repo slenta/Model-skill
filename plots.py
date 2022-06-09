d7d0471f1887d8e7da385f507a68c22ccb83ffc4
slenta <simon.lentz@yahoo.de>
2022-06-09 14:56:09 +0200

    ohc

--- plots.py
+++ plots.py
@@ -48,6 +48,7 @@ def bias_plot(var_1, var_2, del_t, name_1, name_2):
 
     #calculate correlation between both variables
     for j in range(n[1]):
+        print(j)
         for k in range(n[2]):
             
             #calculate running mean, if necessary
