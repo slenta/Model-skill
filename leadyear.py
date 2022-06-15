dd358905b1aeae8d5a37d2b1c908e7e976594e7d
slenta <simon.lentz@yahoo.de>
2022-06-15 11:57:31 +0200

    last changes

--- leadyear.py
+++ leadyear.py
@@ -51,8 +51,11 @@ class calculate_leadyear(object):
         hind = hin.__getitem__()
         hin.plot()
         time, lon, lat = hin.get_coords()    
-        print(hist.shape, hind.shape, obs.shape)
 
+        hist = hist[:, ::-1, :]
+        hind = hind[:, ::-1, :]
+        obs = obs[:, :, ::-1]
+        
         residual_dataset = residual(lead_year, start_year)
         residual_dataset.save_data(obs, hist, hind, time, lon, lat)
 
