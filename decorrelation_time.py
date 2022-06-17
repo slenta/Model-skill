98fc652a19a820a9347300e4d0be60ef823f828a
slenta <simon.lentz@yahoo.de>
2022-06-17 13:12:47 +0200

    last changes

--- decorrelation_time.py
+++ decorrelation_time.py
@@ -75,7 +75,7 @@ class decorrelation_time(object):
         significance = np.array(significance)
 
         plt.figure(figsize=(8, 5))
-        plt.scatter(significance[0], significance[1], c='black')
+        plt.scatter(significance[0], significance[1], 'o', c='black', s=40)
         plt.imshow(decor, cmap='coolwarm', vmin=0, vmax=15)
         plt.xlabel('Longitudes')
         plt.ylabel('Latitudes')
