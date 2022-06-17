13bd86dcb19294b0e3daa5f6107bd0696fdf083f
slenta <simon.lentz@yahoo.de>
2022-06-17 13:31:13 +0200

    last changes

--- decorrelation_time.py
+++ decorrelation_time.py
@@ -80,7 +80,7 @@ class decorrelation_time(object):
         print(significance, np.where(significance!=np.nan))
 
         plt.figure(figsize=(8, 5))
-        plt.scatter(significance[0], significance[1], color='black', s=40, marker='o')
+        plt.plot(significance[0], significance[1], color='black', s=80, marker='o')
         plt.imshow(decor, cmap='coolwarm', vmin=0, vmax=15)
         plt.xlabel('Longitudes')
         plt.ylabel('Latitudes')
