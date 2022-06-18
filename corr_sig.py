2d558426f850dfb6f4ede7f92e334c6123f45355
slenta <simon.lentz@yahoo.de>
2022-06-18 13:55:52 +0200

    significance plot

--- corr_sig.py
+++ corr_sig.py
@@ -85,8 +85,7 @@ def corr_ttest(x, y, alpha=0.05):
 
     Ne = gmean([Nex+Ney])
 
-    if Ne == np.NaN:
-        print(r, x, y)
+    print(r, x, y)
 
     assert Ne >= 10, 'Too few effective d.o.f. to apply this method!'
 
